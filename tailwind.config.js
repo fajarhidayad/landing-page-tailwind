b3baa3ede4eb021d68af617f04c1f968e6f48c6b
Fajar Hidayad <fajarsuryahidayad@gmail.com>
2021-12-28 19:33:09 +0700

    desktop screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Be Vietnam Pro", "sans-serif"],
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+    extend: {
+      colors: {
+        primary: "#F25F3A",
+        secondary: "#242D52",
+      },
+    },
   },
   plugins: [],
 };
